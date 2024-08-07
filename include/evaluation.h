125a7f2956891c854ce88ad04bae543627ebc430
Kevin Feske <kevin.feske@gmail.com>
2024-08-07 21:07:01 +0200

    Piece Square Tables
    =better positional knowledge

--- include/evaluation.h
+++ include/evaluation.h
@@ -2,7 +2,7 @@
 struct Evaluation
 {
 	Phase phase;
-	int evaluate(Board &board, PSQT &)
+	int evaluate(Board &board, PSQT &psqt)
 	{
 		int value = 0;
 		int material = board.non_pawn_material[WHITE] + board.non_pawn_material[BLACK];
@@ -12,10 +12,10 @@ struct Evaluation
 		for (Piece p : { W_PAWN, W_KNIGHT, W_BISHOP, W_ROOK, W_QUEEN, B_PAWN, B_KNIGHT, B_BISHOP, B_ROOK, B_QUEEN }) {
 			uint64_t pieces = board.pieces[p];
 			while (pieces) {
-				//unsigned square = pop_lsb(pieces);
-				pop_lsb(pieces);
-				value += get_piece_value(p, MIDGAME);
-				//value += psqt.psqt[p][square];// * phase; important!!!
+				unsigned square = pop_lsb(pieces);
+				//pop_lsb(pieces);
+				//value += get_piece_value(p, MIDGAME);
+				value += psqt.psqt[p][square];// * phase; important!!!
 				//value += psqt.psqt[ENDGAME][p][square] * (128 - phase);
 			}
 		}
